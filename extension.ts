744a66ddd8cabba5835d6180e63bb77075e01cf8
Frida Viset <frida.viset@gmail.com>
2017-07-31 13:33:13 +0200

    sdF

--- extension.ts
+++ extension.ts
@@ -3,7 +3,6 @@ namespace testStuff {
      * Help goes here.
      */
     //% block
-    //% shim=testStuff::extfun
     export function extfun(x: number, y: number) {
         // Dummy implementation for the simulator.
         return x - y
